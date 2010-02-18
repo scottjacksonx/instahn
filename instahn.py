9ed4c43e61ac148376b52bcd034ae4036199f9ea
Scott Jackson <scottjacksonx@gmail.com>
2010-02-18 10:44:48 +1000

    instahn.py can now accept arguments representing your Instapaper username and password. If no arguments are given, instahn.py will prompt the user for their Instapaper username and password. See README.mdown for more details.

--- instahn.py
+++ instahn.py
@@ -1,13 +1,23 @@
-instapaper.py is Copyright (c) 2009 Eiji Kato
-See http://github.com/technolize/python-instapaper/ for details.
+#instapaper.py is Copyright (c) 2009 Eiji Kato
+#See http://github.com/technolize/python-instapaper/ for details.
 
 from instapaper import *
 from hnapi import *
+import sys
 
-username = ""	# Change this to your own Instapaper username.
-password = ""	# Change this to your own Instapaper password.
+username = ""
+password = ""
 
+args = sys.argv
 
+if len(args) == 1:
+	username = raw_input("Enter your Instapaper username: ")
+	password = raw_input("Enter your Instapaper password (if you don't have a password, just press Return): ")
+elif len(args) == 2:
+	username = args[1]
+elif len(args) == 3:
+	username = args[1]
+	password = args[2]
 
 hn = HackerNewsAPI()
 instapaper = Instapaper(username, password)
